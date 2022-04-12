319787a61eba61321ce1c3d33c22501f92e23670
Michael Pontikes <michael.pontikes_1@nxp.com>
2022-04-12 09:55:45 -0700

    [MICRSE-587] Merge "Video test source" and "Camera Preview" into one demo.

---
+++ scripts/multimedia/gstreamer/video_test.py
@@ -0,0 +1,122 @@
+"""
+Video Test Demo.
+
+Copyright 2022 NXP Semiconductors
+SPDX-License-Identifier: BSD-3-Clause
+
+This demo is meant for users to try out cameras and displays connected to a
+board. This simple UI allows for different cameras to be selected, or if no
+camera is selected, users can use a test source.
+"""
+import glob
+import threading
+import subprocess
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gio, GLib
+
+def run_pipeline(pipeline, type):
+    """Run the pipeline the user selects."""
+    subprocess.run(pipeline.split(' '))
+    GLib.idle_add(Gtk.main_quit)
+
+class MainWindow(Gtk.Window):
+    """Main UI window."""
+
+    def __init__(self):
+        """Create the UI window."""
+        super().__init__()
+        self.set_default_size(450, 200)
+        self.set_resizable(False)
+        self.set_border_width(10)
+        main_grid = Gtk.Grid(
+            row_homogeneous=True, column_spacing=15, row_spacing=15)
+        main_grid.set_margin_end(10)
+        main_grid.set_margin_start(10)
+
+        header = Gtk.HeaderBar()
+        header.set_title("Video Test Demo")
+        header.set_subtitle("GStreamer Demos")
+        self.set_titlebar(header)
+
+        quit_button = Gtk.Button()
+        quit_icon = Gio.ThemedIcon(name="application-exit-symbolic")
+        quit_image = Gtk.Image.new_from_gicon(quit_icon, Gtk.IconSize.BUTTON)
+        quit_button.add(quit_image)
+        header.pack_end(quit_button)
+        quit_button.connect("clicked", Gtk.main_quit)
+
+        source_label = Gtk.Label(
+            label="Source"
+        )
+
+        devices = ["Test Source"]
+        for device in glob.glob('/dev/video*'):
+            devices.append(device)
+        self.source_select = Gtk.ComboBoxText()
+        for option in devices:
+            self.source_select.append_text(option)
+        self.source_select.set_active(0)
+        self.source_select.set_hexpand(True)
+
+        resolution_label = Gtk.Label(
+            label="Resolution"
+        )
+
+        resolutions = ["3840x2160", "2560x1440", "1920x1080",
+        "1280x720", "720x480"]
+        self.resolution_select = Gtk.ComboBoxText()
+        for res in resolutions:
+            self.resolution_select.append_text(res)
+        self.resolution_select.set_active(2)
+
+        self.scale_check = Gtk.CheckButton.new_with_label("Scale to output")
+
+        quit_label = Gtk.Label(
+            label="Once started, users may stop the video feed in the NXP\n"
+            "Demo Experience window by pressing \"Stop Current Demo\".\n"
+            "Playback windows can be dragged with the mouse."
+        )
+
+        self.launch_button = Gtk.Button.new_with_label("Run")
+        self.launch_button.connect("clicked", self.on_start)
+
+        main_grid.attach(source_label, 0, 0, 1, 1)
+        main_grid.attach(self.source_select, 1, 0, 1, 1)
+        main_grid.attach(resolution_label, 0, 1, 1, 1)
+        main_grid.attach(self.resolution_select, 1, 1, 1, 1)
+        main_grid.attach(self.scale_check, 0, 2, 2, 1)
+        main_grid.attach(quit_label, 0, 3, 2, 1)
+        main_grid.attach(self.launch_button, 0, 4, 2, 1)
+
+        self.add(main_grid)
+
+    def on_start(self, widget):
+        """Set up and start the pipeline."""
+        source = self.source_select.get_active_text()
+        if source == "Test Source":
+            source = "videotestsrc"
+        else:
+            source = "v4l2src device=" + source
+        resolution = self.resolution_select.get_active_text()
+        width = resolution[:resolution.find("x")]
+        height = resolution[resolution.find("x")+1:]
+        format = "video/x-raw,width=" + width + ",height=" + height
+        if self.scale_check.get_active():
+            sink = "waylandsink"
+        else:
+            sink = (
+                "waylandsink window-width=" + width + " window-height=" +
+                height)
+        pipeline = "gst-launch-1.0 " + source + " ! " + format + " ! " + sink
+        stream_thread = threading.Thread(
+            target=run_pipeline, args=(pipeline,"gstreamer"))
+        stream_thread.start()
+        self.destroy()
+
+
+if __name__ == "__main__":
+    main_window = MainWindow()
+    main_window.show_all()
+    Gtk.main()
