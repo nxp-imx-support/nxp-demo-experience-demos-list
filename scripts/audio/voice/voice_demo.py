99ea759376f58fa1bd7a4b9e0bc01b00ca748fb1
Michael Pontikes <michael.pontikes_1@nxp.com>
2022-10-18 14:27:01 -0700

    [MICRSE-1015] Add Low Power option

--- scripts/audio/voice/voice_demo.py
+++ scripts/audio/voice/voice_demo.py
@@ -42,7 +42,7 @@ class VoiceGUI(Gtk.Window):
         + WINDOW ,
         "/opt/imx-gpu-sdk/GLES2/S08_EnvMappingRefraction/GLES2.S08_EnvMapping"
         "Refraction_Wayland " + WINDOW,
-        "/opt/viv_samples/tiger/tiger"
+        "/home/root/.nxp-demo-experience/scripts/audio/voice/sleep.sh"
     ]
 
     def __init__(self):
@@ -53,7 +53,7 @@ class VoiceGUI(Gtk.Window):
         self.commands_missing = True
         self.demo = None
 
-        self.set_default_size(450, 200)
+        self.set_default_size(450, -1)
         self.set_resizable(False)
         self.set_border_width(10)
 
@@ -62,26 +62,44 @@ class VoiceGUI(Gtk.Window):
         header.set_subtitle("Voice Solutions")
         self.set_titlebar(header)
 
-        quit_button = Gtk.Button()
+        self.quit_button = Gtk.Button()
         quit_icon = Gio.ThemedIcon(name="process-stop-symbolic")
         quit_image = Gtk.Image.new_from_gicon(quit_icon, Gtk.IconSize.BUTTON)
-        quit_button.add(quit_image)
-        header.pack_end(quit_button)
-        quit_button.connect("clicked", self.quit_demo)
+        self.quit_button.add(quit_image)
+        header.pack_end(self.quit_button)
+        self.quit_button.connect("clicked", Gtk.main_quit)
 
         self.main_grid = Gtk.Grid(
             row_homogeneous=False, column_homogeneous=True,
             column_spacing=15, row_spacing=15)
         self.main_grid.set_margin_end(10)
         self.main_grid.set_margin_start(10)
-        self.status_label = Gtk.Label.new("Setting up...")
-        self.main_grid.attach(self.status_label, 0, 0, 1, 1)
+        self.lpv_button = Gtk.CheckButton.new_with_label(label="Use Cortex-M Core")
+        self.warning_label = Gtk.Label.new(
+            "To use low power voice, please follow the instructions in\n"
+            "the i.MX Linux User's Guide (8.3.4) to set up the Cortex-M\n"
+            "core. The suspend fuction will only suspend the system when\n"
+            "using Cortex-M Core.")
+        self.start_start = Gtk.Button(label="Start")
+        self.status_label = Gtk.Label.new("")
+        self.start_start.connect("clicked", self.kick_off)
+        self.main_grid.attach(self.warning_label, 0, 0, 1, 1)
+        self.main_grid.attach(self.lpv_button, 0, 1, 1, 1)
+        self.main_grid.attach(self.start_start, 0, 2, 1, 1)
+        self.main_grid.attach(self.status_label, 0, 3, 1, 1)
         self.add(self.main_grid)
+
+    def kick_off(self, unused):
+        GLib.idle_add(self.start_start.set_sensitive, False)
+        GLib.idle_add(self.lpv_button.set_sensitive, False)
+        voice_start = threading.Thread(target=self.start_up)
+        voice_start.start()
+
 
     def start_up(self):
         """Sets up VoiceSpot and VoiceSeeker"""
         GLib.idle_add(self.status_label.set_text,
-            "\n\nLoading required modules...")
+            "Loading required modules...")
         res = subprocess.getstatusoutput(
             "modprobe snd-aloop"
         )
@@ -90,7 +108,7 @@ class VoiceGUI(Gtk.Window):
             "\n\nMissing snd-aloop.ko!")
             return
         GLib.idle_add(self.status_label.set_text,
-            "\n\nSetting configuration files...")
+            "Setting configuration files...")
         subprocess.getstatusoutput(
             "cp /etc/asound.conf /etc/asound_org.conf"
         )
@@ -98,19 +116,31 @@ class VoiceGUI(Gtk.Window):
             ['cat', '/sys/devices/soc0/soc_id']
         ).decode('utf-8')[:-1]
         if board == "i.MX8MP":
-            res = subprocess.getstatusoutput(
-                "cp /unit_tests/nxp-afe/asound.conf_imx8mp /etc/asound.conf"
-            )
+            if(self.lpv_button.get_active()):
+                res = subprocess.getstatusoutput(
+                    "cp /unit_tests/nxp-afe/asound.conf_rpmsg_imx8mp /etc/"
+                    "asound.conf"
+                )
+            else:
+                res = subprocess.getstatusoutput(
+                    "cp /unit_tests/nxp-afe/asound.conf_imx8mp /etc/asound.conf"
+                )
         else:
-            res = subprocess.getstatusoutput(
-                "cp /unit_tests/nxp-afe/asound.conf /etc/asound.conf"
-            )
+            if(self.lpv_button.get_active()):
+                res = subprocess.getstatusoutput(
+                    "cp /unit_tests/nxp-afe/asound.conf_rpmsg_imx8mm /etc/"
+                    "asound.conf"
+                )
+            else:
+                res = subprocess.getstatusoutput(
+                    "cp /unit_tests/nxp-afe/asound.conf /etc/asound.conf"
+                )
         if res[0] != 0:
             GLib.idle_add(self.status_label.set_text,
-                "\n\nConfigure files not found!")
+                "Configure files not found!")
             return
         GLib.idle_add(self.status_label.set_text,
-            "\n\nStarting Voice UI and VoiceSpot...")
+            "Starting Voice UI and VoiceSpot...")
         try:
             self.voicespot = subprocess.Popen(
                 ['script', '-q', '-c',
@@ -119,13 +149,13 @@ class VoiceGUI(Gtk.Window):
                 stderr=subprocess.STDOUT)
         except:
             GLib.idle_add(self.status_label.set_text,
-                "\n\nFailed to launch Voice UI!")
+                "Failed to launch Voice UI!")
             return
         vs_watch = threading.Thread(
             target=self.handle_voicespot)
         vs_watch.start()
         GLib.idle_add(self.status_label.set_text,
-            "\n\nStarting VoiceSeekerLite...")
+            "Starting VoiceSeekerLite...")
         try:
             self.voiceseeker = subprocess.Popen(
                 ['/unit_tests/nxp-afe/afe', 'libvoiceseekerlight'],
@@ -133,16 +163,17 @@ class VoiceGUI(Gtk.Window):
                 stderr=subprocess.STDOUT)
         except:
             GLib.idle_add(self.status_label.set_text,
-                "\n\nFailed to start VoiceSeekerLite")
+                "Failed to start VoiceSeekerLite")
             return
         vse_watch = threading.Thread(
             target=self.handle_voiceseeker)
         vse_watch.start()
         GLib.idle_add(self.status_label.set_text,
-            "\n\nFinishing up...")
+            "Finishing up...")
         # Wait until commands are read from VIT model
         while(self.commands_missing):
             time.sleep(0.1)
+        self.quit_button.connect("clicked", self.quit_demo)
         GLib.idle_add(self.start_main)
     
     def start_main(self):
@@ -155,11 +186,15 @@ class VoiceGUI(Gtk.Window):
         phrases_label = Gtk.Label.new(command_string[:-1])
         self.command_label =  Gtk.Label.new("Last command:\nN/A")
         self.status_label =  Gtk.Label.new("Waiting for wake word...")
-        self.main_grid.remove_row(0)
-        self.main_grid.attach(ww_label, 0, 0, 1, 1)
-        self.main_grid.attach(phrases_label, 0, 1, 1, 1)
-        self.main_grid.attach(self.command_label, 1, 0, 1, 1)
-        self.main_grid.attach(self.status_label, 1, 1, 1, 1)
+        self.main_grid2 = Gtk.Grid(
+            row_homogeneous=False, column_homogeneous=True,
+            column_spacing=15, row_spacing=15)
+        self.main_grid2.attach(ww_label, 0, 0, 1, 1)
+        self.main_grid2.attach(phrases_label, 0, 1, 1, 1)
+        self.main_grid2.attach(self.command_label, 1, 0, 1, 1)
+        self.main_grid2.attach(self.status_label, 1, 1, 1, 1)
+        self.remove(self.main_grid)
+        self.add(self.main_grid2)
         self.show_all()
         
     def handle_voicespot(self):
@@ -193,9 +228,15 @@ class VoiceGUI(Gtk.Window):
                     if self.demo != None:
                             self.demo.terminate()
                             self.demo = None
-                    if wordarr[0] != "12":
+                    if wordarr[0] != "12" and wordarr[0] != "11":
                         self.demo = subprocess.Popen(
                             self.EXE[int(wordarr[0])-1].split())
+                    if wordarr[0] == "11":
+                        if(self.lpv_button.get_active()):
+                            subprocess.Popen(
+                                self.EXE[int(wordarr[0])-1].split())
+                        else:
+                            word = word + " (Skipped)"
                 GLib.idle_add(self.command_label.set_text,
                     "Last command:\n" + word)
                 GLib.idle_add(self.status_label.set_text,
@@ -233,9 +274,6 @@ def main():
     """Starts the demo"""
     gui = VoiceGUI()
     gui.show_all()
-    voice_start = threading.Thread(
-        target=gui.start_up)
-    voice_start.start()
     Gtk.main()
 
 if __name__ == "__main__":
